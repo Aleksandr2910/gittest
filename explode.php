1dc948ff391979198f65fd95d68bb0cf845dff95
Aleksandr Stankevich <aleks-stankevich@yandex.ru>
2018-05-05 18:13:49 +0500

    4to-to-polu4aetsya

---
+++ explode.php
@@ -0,0 +1,8 @@
+<?php
+$test = "я считаю до пяти";
+$vivod = explode(" ", $test);
+echo $vivod[0]."<br>";
+print $vivod[1]."<br>";
+echo $vivod[3]."<br>";
+
+?>
---
+++ implode.php
@@ -0,0 +1,9 @@
+<?php
+$a = "я";
+$b = "иду";
+$c = "гулять";
+$massiv = array($a,$b,$c);
+$vmeste = implode(" ", $massiv);
+
+echo $vmeste;
+?>
